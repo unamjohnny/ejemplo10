2aa2821d37dbe6848e176e10b1e9109b83625911
unamjohnny <accigamer77@hotmail.com>
2017-11-27 23:34:03 -0600

    Tercer commit (unidad1_actividad_2)

--- index.php
+++ index.php
@@ -2,11 +2,22 @@
 
 require_once __DIR__.'/vendor/autoload.php';
 
+use GuzzleHttp\Client;
+use Symfony\Component\HttpFoundation\Response;
+
+$api_key = getenv('OPEN_WEATHER_API');
+
 $app = new Silex\Application();
 
-$app->get('/clima', function() use($app) {
-$arreglo = ["Juan Carlos Carranza Moreno" => "415074876"];
-    return $app->json($arreglo);
+$app->get('/clima', function() use($app,$api_key) {
+    
+    $client = new Client();
+    $url ="http://api.openweathermap.org/data/2.5/weather?id=3530597&appid=$api_key&units=metric";
+    $response = $client->get($url);
+    $body = $response->getBody();
+
+    return new Response($body,200,array('Content-Type' => 'application/json'));
+
 });
 
 $app->run();
